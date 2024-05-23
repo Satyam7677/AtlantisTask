f663683704a8fa37d4c1eafd39c400ee947b7bc3
Satyam Kumar Tiwari <53082021+Satyam7677@users.noreply.github.com>
2024-05-23 12:26:36 +0530

    Update AppString.ts

--- src/utilities/AppString.ts
+++ src/utilities/AppString.ts
@@ -5,4 +5,4 @@ export default {
     geolocationEndpoint:(lat:any,long:any)=>`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&sensor=false&key=${apiKey}`
 
 }
-const apiKey='AIzaSyADb8oLVP_h3zijBuuN3ox8ha0C9lw0O1I'
+const apiKey='YOUR_API_KEy'
